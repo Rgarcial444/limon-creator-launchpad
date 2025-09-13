d29eb129a6bb88511c5ef51670a8e566bf79fa14
Rgarcial444 <rgarcialimon@gmail.com>
2025-09-12 20:05:36 -0600

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -27,7 +27,7 @@ const Hero = () => {
         transition={{
           duration: 0.6,
         }}
-        className="z-50 flex flex-col justify-center items-center text-center px-6"
+        className="h-screen brightness-125 saturate-150 contrast-110"
       >
         <motion.h1 className="font-bold text-4xl md:text-6xl lg:text-7xl text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 py-4 leading-tight">
           Dale Vida a Tu Marca
