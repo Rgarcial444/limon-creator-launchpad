ca29f6664df158431530f9f92cfc339779792927
Rgarcial444 <rgarcialimon@gmail.com>
2025-09-26 13:43:29 -0600

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,16 +1,10 @@
 import Hero from '@/components/Hero';
-import About from '@/components/About';
-import Services from '@/components/Services';
-import Contact from '@/components/Contact';
 import Footer from '@/components/Footer';
 
 const Index = () => {
   return (
     <div>
       <Hero />
-      <About />
-      <Services />
-      <Contact />
       <Footer />
     </div>
   );
